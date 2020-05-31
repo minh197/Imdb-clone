40166427ddbdfc4f19bfca8c26c370219f86c869
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-05-31 06:34:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,192 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import "./App.css";
+import myMovie from "./components/Movie"
+import Movie from "./components/Movie";
+import keyword from "./components/Movie";
+
+
+
+let apiKey = process.env.REACT_APP_APIKEY
 
 function App() {
+  let [movieList, setMovieList] = useState(null)
+  let [topratedList, setTopRatedList] = useState(null)
+  let [originalList,setOriginalList]=useState(null)
+
+  const getTopRatedMovie = async () =>{
+    let url =`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
+    let data = await fetch(url)
+    let result = await data.json()
+    setMovieList(result.results)
+  }
+
+
+  const rattingFilm = () =>{
+  
+    let filmRattingList = [];
+   
+    let filmRatting = movieList.sort((a,b)=>{
+      if(a.vote_average>b.vote_average) return 1
+      if(a.vote_average<b.vote_average) return -1
+      if(a.vote_average=b.vote_average) return 0
+    })
+      // filmRattingList.push(filmRatting);
+      console.log("Ratting film here", filmRatting);
+
+    setMovieList([...filmRatting]);
+  }
+
+
+
+  const rattingToHigh = () =>{
+  
+    let filmRattingList = [];
+   
+    let filmRatting = movieList.sort((a,b)=>{
+      if(a.vote_average>b.vote_average) return -1
+      if(a.vote_average<b.vote_average) return 1
+      if(a.vote_average=b.vote_average) return 0
+    })
+      // filmRattingList.push(filmRatting);
+      
+
+    setMovieList([...filmRatting]);
+  }
+
+  //popularity
+
+  const popularityFilm = () =>{
+  
+    let lowPopularityList = [];
+   
+    let filmRatting = movieList.sort((a,b)=>{
+      if(a.popularity>b.popularity) return 1
+      if(a.popularity<b.popularity) return -1
+      if(a.popularity=b.popularity) return 0
+    })
+      // filmRattingList.push(filmRatting);
+      
+
+    setMovieList([...filmRatting]);
+  }
+
+  const highFilm = () =>{
+  
+    let highPopularityList = [];
+   
+    let filmRatting = movieList.sort((a,b)=>{
+      if(a.popularity>b.popularity) return -1
+      if(a.popularity<b.popularity) return 1
+      if(a.popularity=b.popularity) return 0
+    })
+      // filmRattingList.push(filmRatting);
+      
+
+    setMovieList([...filmRatting]);
+  }
+
+
+
+
+  const getNowPlayMovie = async () => {
+
+    let url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
+    let data = await fetch(url)
+    let result = await data.json()
+    setOriginalList(result.results)
+    setMovieList(result.results)
+    //newRelease(result.results)
+    console.log("movie", result)
+
+  }
+  const backHome = ()=>{
+    setMovieList(originalList)
+    
+  }
+
+  const searchFilm = (keyword)=>{
+    let movieSearching=[];
+    if(keyword === ''){
+      setMovieList(originalList)
+      return;
+    }
+    for(let i=0; i<movieList.length;i++){
+      let filmSearching = movieList[i]
+      if(movieList[i].title.toLowerCase().includes(keyword.toLowerCase())){
+        movieSearching.push(filmSearching);
+        console.log("what is film searching", movieSearching)
+
+
+      }
+    }
+    setMovieList(movieSearching)
+  }
+  const newRelease =()=>{
+    console.log("I am here")
+   
+    let nowplayingFilm =[];
+    //get the whole move list 
+    // from taht get each item 
+    // from each item, we get the release_Date 
+    // from that release date, we split by -
+    // we grab the first index balue which is year info
+    // we comopare that ear value is  >=2020
+    // if thats true, we push into nowplayingFilm array
+    // set that now plaing Film array into state so user can see 
+
+    for(let i=0; i<movieList.length; i++){
+      let newRelease = movieList[i];
+      
+      let newRealeseDate = newRelease.release_date;
+      
+      let myfilmList = newRealeseDate.split("-")
+      let year = myfilmList[0]
+      let month = myfilmList[1]
+      
+      if(year>=2020 && month>=2){
+        
+          nowplayingFilm.push(newRelease)
+
+      }
+ 
+    }
+
+    
+
+  
+    setMovieList(nowplayingFilm);
+
+    
+
+  
+  }
+
+  useEffect(() => {
+    getNowPlayMovie();
+  }, [])
+
+  
+
+
+  if (movieList === null) {
+    return (<div>Loading</div>)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Movie myMovieList={movieList} onNewRelease = {newRelease} topList={getTopRatedMovie} topSearch={searchFilm } comeBack={backHome} lowToHigh={rattingFilm} highToLow={rattingToHigh} lowPop={popularityFilm} highPop={highFilm}/>
+
+     
     </div>
   );
-}
-
+  
+  }
 export default App;
+//create new array for film that releases on 2020
+
+
+
+//create a 2020film function
+
+
+
+//link that function with currently playing film
---
+++ src/components/Movie.js
@@ -0,0 +1,137 @@
+import React, { useState } from 'react'
+import { Card, ListGroup, Navbar, Nav, Form, FormControl, Button, ListGroupItem, Row, Col, Container, Dropdown,Jumbotron } from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './../App.css'
+export default function Movie(props) {
+    // let [keyword,setKeyword] = useState('')
+    let keyword = ''
+    const searchBtn = (e) => {
+        e.preventDefault();
+
+        console.log("whats the search term", keyword)
+        props.topSearch(keyword);
+    }
+    let htmlMovie = props.myMovieList.map((movie) => {
+        let myMovie = movie;
+        return (
+
+
+            
+
+
+                <Card className="col-md-6 col-lg-4 p-3 card  ">
+                    <div data-test="container" class="container">
+                        <div class="view overlay zoom" data-test="view">
+                            <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} />
+                        </div>
+
+                        <div class="card mb-5 box-shadow">
+
+                            <Card.Body>
+                                <Card.Title>{movie.title}</Card.Title>
+                                <Card.Text>
+                                    {movie.overview}
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>Vote average: {movie.vote_average}</ListGroupItem>
+                                <ListGroupItem>Release Date: {movie.release_date}</ListGroupItem>
+                                <ListGroupItem>Popularity: {movie.popularity}</ListGroupItem>
+                            </ListGroup>
+                            <Card.Body>
+
+
+                            </Card.Body>
+                        </div>
+
+                    </div>
+                </Card>
+            
+
+
+
+
+
+        )
+    })
+    return (
+        <div class="container-fluid sm">
+            
+                <nav class="fixed-top navbar navbar-expand-lg navbar-dark bg-dark " >
+                    <Navbar bg="dark" expand="lg">
+                        <Navbar.Brand href="#home" onClick={() => props.comeBack()}>HuFilm</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+
+                            <Nav className="mr-auto">
+                                <Nav.Link href="#home" onClick={() => props.onNewRelease()}>Latest</Nav.Link>
+                                <Nav.Link href="#link" onClick={() => props.topList()}>Top Rated</Nav.Link>
+                                <Dropdown>
+                                    <Dropdown.Toggle href="#Ratting" variant="dark" id="dropdown-basic">
+                                        Ratting
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1" onClick={() => props.lowToHigh()}>Lowest to Highest</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2 " onClick={() => props.highToLow()}>Highest to Lowest</Dropdown.Item>
+
+                                    </Dropdown.Menu>
+                                </Dropdown>
+
+
+                                <Dropdown>
+                                    <Dropdown.Toggle href="#Ratting" variant="dark" id="dropdown-basic">
+                                        Popularity
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1" onClick={() => props.lowPop()}>Lowest to Highest</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2 " onClick={() => props.highPop()}>Highest to Lowest</Dropdown.Item>
+
+                                    </Dropdown.Menu>
+                                </Dropdown>
+
+                            </Nav>
+                            <Form inline onSubmit={(e) => searchBtn(e)}>
+                                <FormControl type="text" placeholder="Search" className="mr-sm-2 form-control" onChange={(e) => keyword = e.target.value} />
+                                <Button variant="outline-info" type="submit">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </nav>
+            <section>
+            <div className="Minh-wrapper">
+                <Jumbotron fluid>
+                <div class="bg-img">
+                    <div class="content-wrapper">
+                    <Container>
+                        
+                        
+                    
+                    </Container>
+                    </div>
+                </div>
+                </Jumbotron>
+            </div>
+            </section>
+            <div className="d-flex flex-wrap main justify-content-center">
+                <Container>
+
+                    <Col>
+
+                        <Row></Row>
+                        <Row xs={12}>{htmlMovie}</Row>
+                        <Row></Row>
+
+                    </Col>
+                </Container>
+
+
+
+
+
+            </div>
+        </div>)
+
+}
